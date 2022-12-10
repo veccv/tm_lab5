917d89246cc3512e552fbbf5261972a67de6d82b
Jacek <jacdal000@pbs.edu.pl>
2022-12-10 17:55:50 +0100

    Changed method to connect to database

--- add_user.php
+++ add_user.php
@@ -1,30 +1,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <HEAD>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </HEAD>
 <BODY>
 <?php
-$user = htmlentities ($_POST['user'], ENT_QUOTES, "UTF-8");
-$pass = htmlentities ($_POST['pass'], ENT_QUOTES, "UTF-8");
-$pass_repeat = htmlentities ($_POST['pass_repeat'], ENT_QUOTES, "UTF-8");
+include 'Database.php';
+$user = htmlentities($_POST['user'], ENT_QUOTES, "UTF-8");
+$pass = htmlentities($_POST['pass'], ENT_QUOTES, "UTF-8");
+$pass_repeat = htmlentities($_POST['pass_repeat'], ENT_QUOTES, "UTF-8");
 
-$auth_file = file_get_contents('auth.txt');
-$lines = explode("\n", $auth_file);
-$host = substr($lines[0], 5);
-$username = substr($lines[1], 9);
-$password = substr($lines[2], 9);
-$database = substr($lines[3], 7);
-$link = mysqli_connect($host, $username, $password, $database);
-if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); } // obsługa błędu połączenia z BD
-mysqli_query($link, "SET NAMES 'utf8'"); // ustawienie polskich znaków
-$result = mysqli_query($link, "SELECT * FROM user WHERE login='$user'"); // wiersza, w którym login=login z formularza
-$rekord = mysqli_fetch_array($result); // wiersza z BD, struktura zmiennej jak w BD
-if(!$rekord) //Jeśli brak, to nie ma użytkownika o podanym loginie
+
+if (!Database::getConnection()) {
+    echo "Błąd: " . mysqli_connect_errno() . " " . mysqli_connect_error();
+}
+Database::getConnection()->query("SET NAMES 'utf8'");
+
+$rekord = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM user WHERE login='$user'"));
+if (!$rekord)
 {
-    // Sprawdź czy hasła się zgadzają
     if ($pass == $pass_repeat) {
-        mysqli_query($link, "INSERT INTO user (login, password) VALUES ('$user', '$pass')");
-        mysqli_close($link);
+        Database::getConnection()->query("INSERT INTO user (login, password) VALUES ('$user', '$pass')");
+        Database::getConnection()->close();
         echo "Zarejestrowano użytkownika $user z hasłem $pass";
 
     } else {
--- weryfikuj3.php
+++ weryfikuj3.php
@@ -1,34 +1,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <HEAD>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </HEAD>
 <BODY>
 <?php
-$user = htmlentities ($_POST['user'], ENT_QUOTES, "UTF-8");
-$pass = htmlentities ($_POST['pass'], ENT_QUOTES, "UTF-8");
+include 'Database.php';
 
-$auth_file = file_get_contents('auth.txt');
-$lines = explode("\n", $auth_file);
-$host = substr($lines[0], 5);
-$username = substr($lines[1], 9);
-$password = substr($lines[2], 9);
-$database = substr($lines[3], 7);
-$link = mysqli_connect($host, $username, $password, $database);
-if(!$link) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); }
-mysqli_query($link, "SET NAMES 'utf8'");
-$result = mysqli_query($link, "SELECT * FROM user WHERE login='$user'");
-$rekord = mysqli_fetch_array($result);
+$user = htmlentities($_POST['user'], ENT_QUOTES, "UTF-8");
+$pass = htmlentities($_POST['pass'], ENT_QUOTES, "UTF-8");
+
+if (!Database::getConnection()) {
+    echo "Błąd: " . mysqli_connect_errno() . " " . mysqli_connect_error();
+}
+
 
 session_start();
 $last_unsuccessful_login = $_SESSION['last_unsuccessful_login'];
 $time_now = time();
 
-
 if (($last_unsuccessful_login + 60) < $time_now) {
+    Database::getConnection()->query("SET NAMES 'utf8'");
+    $rekord = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM user WHERE login='$user'"));
+
     if (!$rekord) //Jeśli brak, to nie ma użytkownika o podanym loginie
     {
         $_SESSION['last_unsuccessful_login'] = time();
-        mysqli_close($link); // zamknięcie połączenia z BD
+        Database::getConnection()->close();
         header('Location: index3.php');
         exit();
     } else { // jeśli $rekord istnieje
@@ -42,7 +39,7 @@ if (($last_unsuccessful_login + 60) < $time_now) {
             header('Location: index4.php');
         } else {
             $_SESSION['last_unsuccessful_login'] = time();
-            mysqli_close($link);
+            Database::getConnection()->close();
             header('Location: index3.php');
             exit();
         }
