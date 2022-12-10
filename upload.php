4d73074a15769c6abcaebb0291b311f6867e4fb9
Jacek <jacdal000@pbs.edu.pl>
2022-12-10 18:51:53 +0100

    Added uploading file with additional information

--- upload.php
+++ upload.php
@@ -1,6 +1,18 @@
 <?php
+include 'Database.php';
+session_start();
+
 $target_dir = "songs";
 $target_file = $target_dir . "/" . basename($_FILES["fileToUpload"]["name"]);
+$file_name = basename($_FILES["fileToUpload"]["name"]);
+
+$title = $_POST['title'];
+$musician = $_POST['musician'];
+$lyrics = $_POST['lyrics'];
+$musictype_id = $_POST['musictype'];
+
+$login = $_SESSION['user'];
+$idu = mysqli_fetch_array(Database::getConnection()->query("SELECT * FROM user WHERE login='$login'"))[0];
 
 if (preg_match('/.mp3$/', $target_file)) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -8,6 +20,11 @@ if (preg_match('/.mp3$/', $target_file)) {
     } else {
         echo "Error uploading file.";
     }
+
+    Database::getConnection()->query("INSERT INTO song (title, musician, idu, filename, lyrics, idmt) VALUES ('$title', '$musician', '$idu', '$file_name', '$lyrics', '$musictype_id')");
+    Database::getConnection()->close();
+
+
     header('Location: index4.php');
 
 } else {
--- upload_file.php
+++ upload_file.php
@@ -2,8 +2,27 @@
 <html>
 <body>
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select file to upload:
+    Select file to upload: <br><br>
     <input type="file" name="fileToUpload" id="fileToUpload">
+    <br><br>
+    <label for="title">Tytuł utworu: </label><input type="text" name="title" id="title">
+    <br>
+    <label for="musician">Autor: </label><input type="text" name="musician" id="musician">
+    <br><br>
+    <label for="lyrics">Tekst piosenki:</label><br>
+    <textarea rows="10" cols="100" id="lyrics" name="lyrics" placeholder="Wpisz tekst piosenki tutaj..."></textarea>
+    <br>
+    <label for="musictype">Wybierz gatunek utworu: </label>
+    <select id="musictype" name="musictype">
+        <?php
+        include 'Database.php';
+        $rekord = mysqli_fetch_all(Database::getConnection()->query("SELECT * FROM musictype"));
+
+        foreach ($rekord as $value) {
+            echo "<option value=" . $value[0] . ">" . $value[1] . "</option>";
+        }
+        ?>
+    </select><br><br>
     <input type="submit" value="Upload" name="submit">
 </form>
 <br>
